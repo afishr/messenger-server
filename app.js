a0654ad955ffe9b7342fd10b5697afa6a4348f38
Alexandru Calugari <afishr00@gmail.com>
2020-11-29 02:08:16 +0200

    Set up eslint

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
+
 const app = express();
-const db = require('./db');
 
 const port = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
-})
+});
