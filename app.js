25ab40dd807930e73c136c0f7bd63cbd15752b82
Alexandru Calugari <afishr00@gmail.com>
2020-11-29 03:16:45 +0200

    Update docker container

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 
 const app = express();
-
 const port = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
 
 app.get('/', (req, res) => {
