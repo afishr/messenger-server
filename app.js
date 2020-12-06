5978e64df1cf8a66d937f61b613c95434c9b89fe
dominatorfaf <dominicflocea.dev@gmail.com>
2020-12-06 14:56:33 +0200

    Added cors

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const { usersRoute } = require('./routes/users.route');
 const { authGuard } = require('./middlewares/auth.middleware');
+const cors = require('cors')
 require('./db');
 
 const app = express();
 const port = process.env.PORT || 8080;
-
+app.use(morgan('common'))
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use('/users', usersRoute);
+app.use(cors({
+  origin: 'http://localhost:3000',
+}))
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
@@ -20,3 +25,9 @@ app.listen(port, () => {
 app.get('/me', authGuard, (req, res) => {
   res.send(req.user);
 });
+
+app.get('/', (req, res) => {
+  res.json({
+    message: "I am useful"
+  })
+})
