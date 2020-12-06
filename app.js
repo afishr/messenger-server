ae77630f6c30879138513b3aa29f7708058e257d
diana <gaponcic.d@gmail.com>
2020-12-06 22:04:58 +0200

    add sockets

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const socketio = require("socket.io");
+const http = require("http");
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const { usersRoute } = require('./routes/users.route');
 const { authGuard } = require('./middlewares/auth.middleware');
+
+const formatMessage = require("./utils/messages");
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require("./utils/users");
+
 const cors = require('cors')
 require('./db');
 const morgan = require('morgan')
@@ -10,8 +16,6 @@ const morgan = require('morgan')
 const app = express();
 app.use(morgan('common'))
 
-const port = process.env.PORT || 8080;
-app.use(morgan('common'))
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/users', usersRoute);
@@ -19,11 +23,13 @@ app.use(cors({
   origin: 'http://localhost:3000',
 }))
 
+const botName = "Chat"
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
+server = app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
 
 app.get('/me', authGuard, (req, res) => {
   res.send(req.user);
@@ -34,3 +40,58 @@ app.get('/', (req, res) => {
     message: "I am useful"
   })
 })
+
+const io = socketio(server)
+
+io.use(function(socket, next){
+  if (socket.handshake.query && socket.handshake.query.token){
+    jwt.verify(socket.handshake.query.token, 'secret', function(err, decoded) {
+      if (err) return next(new Error('Authentication error'));
+      socket.decoded = decoded;
+      next();
+    });
+  }
+  else {
+    next(new Error('Authentication error'));
+  }  
+  next()
+})
+
+io.on("connection", socket => {
+  socket.on("disconnect", () => {
+      const user = userLeave(socket.id);
+      
+      if (user) {
+          io.to(user.room).emit("message", formatMessage(botName, `${user.username} left the chat`));
+
+          io.to(user.room).emit("roomUsers", {
+              room: user.room,
+              users: getRoomUsers(user.room)
+          })
+      }
+  });
+
+  socket.on("chatMessage", (msg) => {
+      console.log("i have a new message", msg)
+      const user = getCurrentUser(socket.id)
+      io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  socket.on("joinRoom", ({ username, room }) => {
+      const user = 
+      console.log(`User ${username} joined rooms ${room}`)
+      const user = userJoin(socket.id, username, room)
+      socket.join(user.room);
+
+      socket.emit("message", formatMessage(botName, "Welcome to our mega secure chat"));
+
+      socket.broadcast
+      .to(user.room)
+      .emit("message", formatMessage(botName, `${user.username} connected`));
+
+      io.to(user.room).emit("roomUsers", {
+          room: user.room,
+          users: getRoomUsers(user.room)
+      })
+  });
+});
