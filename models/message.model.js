7789b6bd78d9629431d7c476107bead4c49a3eef
diana <gaponcic.d@gmail.com>
2020-12-09 21:18:52 +0200

    fixed message model

--- models/message.model.js
+++ models/message.model.js
@@ -5,9 +5,9 @@ const { Schema } = mongoose;
 const messageSchema = new Schema({
   from: { type: mongoose.Schema.Types.ObjectId, required: true },
   chat: { type: mongoose.Schema.Types.ObjectId, required: true },
-  content: { type: String, required: true, index: { unique: true } },
+  content: { type: String, required: true },
   timeSent: { type: Date, required: true, default: Date.now()},
   status: {type: String, enum : ['Sent','Read'], default: 'Sent' },
 });
 
-exports.MessageModel = mongoose.model('User', messageSchema);
+exports.MessageModel = mongoose.model('Message', messageSchema);
