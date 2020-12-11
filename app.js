3b42d5dfbaed3ec4e71885c0a8efc0b120b83ffd
diana <gaponcic.d@gmail.com>
2020-12-11 20:49:11 +0200

    add rate limiting

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ const { chatRoute } = require('./routes/chat.route');
 const { errorHandler } = require('./common/error.handling/error.handler');
 const { formatMessage, getChat, addMessage } = require("./services/chats.service");
 const { getUserId, findUserById } = require("./services/users.service");
+const rateLimit = require("express-rate-limit");
+const { RateLimiterMemory } = require('rate-limiter-flexible');
+
+const httpLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 30,
+});
+
+const socketLimiter = new RateLimiterMemory(
+  {
+    points: 10,
+    duration: 1,
+  });
+
+
 require('dotenv').config();
 require('./db');
 
@@ -17,10 +32,12 @@ app.use(morgan('common'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 // app.use(cors({
 //   origin: 'http://localhost:3000',
 // }));
 
+app.use(httpLimiter);
 app.use(cors());
 app.use('/auth', authRoute);
 app.use('/users', userRoute);
@@ -37,35 +54,41 @@ const io = socketio(server);
 
 io.on("connection", socket => {
   socket.on("disconnectMe", ({chatId}) => {
-      console.log("i disconneced")
+      console.log("i disconnect")
       socket.leave(chatId);
   });
 
 
   socket.on("chatMessage", async ({ msg, token, chatId }) => {
     try {
+      await socketLimiter.consume(socket.handshake.address);
       if (chatId) {
         console.log("i `have a new message", msg, chatId);
-        time = new Date().getTime();
-        userId = getUserId(token);
+        const time = new Date().getTime();
+        const userId = getUserId(token);
         const user = await findUserById(userId);
         await addMessage(user, chatId, msg, time);
         io.to(chatId).emit("message", formatMessage(user.username, msg, time));
       }
-    } catch(e) {console.log("here")}
+    } catch(rejRes) {
+      io.to(chatId).emit('blocked', { 'retry-ms': rejRes.msBeforeNext });
+    }
   });
 
 
   socket.on("joinRoom", async ({ token, to }) => {
     try {
-      userId = getUserId(token);
+      await socketLimiter.consume(socket.handshake.address);
+
+      const userId = getUserId(token);
       if (userId) {
-        user = await findUserById(userId);
-        chatId = await getChat(userId, to);
+        const chatId = await getChat(userId, to);
         socket.join(chatId);
         console.log(`User ${userId} connected to ${chatId}`);
         socket.emit("chatId", chatId);
       }
-    } catch(e) {}
+    } catch(rejRes) {
+      socket.emit('blocked', { 'retry-ms': rejRes.msBeforeNext })
+    }
   });
 });
